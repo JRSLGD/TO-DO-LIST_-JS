224e5b792cac64f187adfb1c4cd9592017c2a239
JRSLGD <jorisgbelia@gmail.com>
2025-08-29 16:03:41 +0000

    commit up

---
+++ script.js
@@ -0,0 +1,12 @@
+function newTask(){
+    const taskInput = document.getElementById("nouvelleTache");
+    const taskList = document.getElementById("listeTaches");
+    const task = taskInput.value.trim();
+
+    if(task !== '') {
+        li = document.createElement('li');
+        li.textContent = task;
+        taskList.appendChild(li);
+        taskInput.value = '';
+    }
+}
